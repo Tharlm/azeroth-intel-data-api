5d4f560c01f8d06b41a6dc436fc504a7a1a77de8
patrick <patrick.bavencoff@yahoo.fr>
2025-04-20 22:11:28 +0200

    init project

---
+++ src/main/java/fr/bavencoff/wow/azerothinteldataapi/AzerothIntelDataApiApplication.java
@@ -0,0 +1,13 @@
+package fr.bavencoff.wow.azerothinteldataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzerothIntelDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzerothIntelDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/bavencoff/wow/azerothinteldataapi/AzerothIntelDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bavencoff.wow.azerothinteldataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzerothIntelDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
