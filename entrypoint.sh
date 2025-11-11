0926c68ba050b96504f5e67b955f72d250472cf6
patrick <patrick.bavencoff@yahoo.fr>
2025-11-11 21:51:55 +0100

    revue deploiement

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 export SPRING_DATASOURCE_USERNAME=$(cat /run/secrets/postgres_user)
 export SPRING_DATASOURCE_PASSWORD=$(cat /run/secrets/postgres_password)
-export SPRING_DATASOURCE_URL=jdbc:postgresql://azeroth-postgres:5432/$(cat /run/secrets/postgres_db)
+export SPRING_DATASOURCE_URL=jdbc:postgresql://tharlm-postgres:5432/$(cat /run/secrets/postgres_db)
 
 exec java -jar /app/app.jar
